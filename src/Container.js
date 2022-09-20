db5a55052d90801b3b557d14f1f0cee8620af1b9
canSir21 <alex.mwaura@moringaschool.com>
2022-09-20 11:09:07 +0300

    create table

--- src/Container.js
+++ src/Container.js
@@ -1,13 +1,15 @@
-const Container = (props) => (
+function Container(props) {
+  return (
     <div
       style={{
-        width: "1200px",
-        margin: "0 auto",
-        textAlign: "center"
+        width: '1200px',
+        margin: '0 auto',
+        textAlign: 'center',
       }}
     >
       {props.children}
     </div>
   );
-  
+}
+
 export default Container;
